3c04ea95ecc1d353c3499d53b931fe27bd9a98ca
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-03-14 17:54:16 +0900

    header, footerの作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import jquery from "jquery"
+window.$ = jquery
+
+$(function(){
+  // profileの表示・非表示
+  var $userMenuButton = $("#js-user-menu-button");
+  $userMenuButton.on('click', function() {
+    console.log(111);
+    $(this).next().slideToggle(200)
+  })
+})
