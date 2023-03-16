33d5dc93c8785cda0a9da03735391d6a000b91ee
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-03-16 13:06:52 +0900

    Userダミーデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p '==================== admin user create ===================='
+User.create!(
+  name: 'taku300',
+  email: 'fukuda.takuma695@mail.kyutech.jp',
+  password: "password",
+  password_confirmation: "password",
+  introduction: 'fippo map作成中！！',
+  is_published: true,
+  avatar: File.open('./app/assets/images/user_placeholder.png')
+)
+p '==================== secret user create ==================='
+User.create!(
+  name: Faker::Name.unique.name,
+  email: Faker::Internet.unique.email,
+  password: "password",
+  password_confirmation: "password",
+  introduction: Faker::Lorem.paragraph,
+  is_published: false,
+)
+p '==================== published user create ================'
+10.times do |n|
+  User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password: "password",
+    password_confirmation: "password",
+    introduction: Faker::Lorem.paragraph,
+    is_published: true,
+  )
+end
+# POST_NUMBER = 10
+# posts = []
+# POST_NUMBER.times do |n|
+#   time = Time.current
+#   posts <<
+#     {
+#       name: Faker::Name.unique.name,
+#     }
+# end
+# Post.insert_all! users, returning: nil
