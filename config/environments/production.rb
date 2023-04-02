432f739801144a5d496dbeb93294c718a1a03880
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-04-02 20:07:50 +0900

    メール修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_API_KEY'],
-  :domain => 'herokuapp.com',
+  :domain => 'www.fippo-map.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
