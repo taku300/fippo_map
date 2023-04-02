53a6e07f84bb80bcdb5982921d32b976888172c0
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-04-02 19:52:43 +0900

    メール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "fippo_map_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  :user_name      => 'apikey',
+  :password       => ENV['SENDGRID_API_KEY'],
+  :domain => 'herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
