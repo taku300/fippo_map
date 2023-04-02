d92134ba9f0aa88776bf1d0bd69c84848338aa69
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-04-02 20:16:08 +0900

    メール修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
+    domain: "www.fippo-map.com",
     address: "smtp.SendGrid.net",
     port: 587,
     authentication: :plain,
