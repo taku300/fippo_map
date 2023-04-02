d96624c58b8c057ca4be4a604f3eaae3c8dae136
taku300 <fukuda.takuma695@mail.kyutech.jp>
2023-04-02 22:13:04 +0900

    メール修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :smtp
-  host = 'https://fippo-map.herokuapp.com/'
+  host = 'https://www.fippo-map.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
